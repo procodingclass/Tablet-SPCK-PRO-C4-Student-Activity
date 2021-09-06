ba7b3fefaea9fc3cf96165687022442fa70da6cf
procodingclass@whitehatjr.com <procodingclass@whitehatjr.com>
2021-09-06 11:01:57 +0530

    Revamped version 1

--- sketch.js
+++ sketch.js
@@ -28,20 +28,18 @@ function setup() {
 
   //generate random numbers
   var rand = Math.round(random(1, 100));
-  console.log(rand);
 }
 
 function draw() {
   //set background color
   background(180);
 
-  console.log(trex.y);
-
   // jump when the space key is pressed
   if (keyDown("space") && trex.y >= 100) {
     trex.velocityY = -12;
   }
 
+  // Add gravity
   trex.velocityY = trex.velocityY + 0.8;
 
   if (ground.x < 0) {
@@ -60,4 +58,24 @@ function draw() {
 //function to spawn the clouds
 function spawnClouds() {
   // write your code here
+  //writes the code to generate a cloud for every 60 frames
+  // Hint : Checkout step 2 image inside screenshots folder
+
+
+    //Create cloud sprite and add velocity to it
+    // Hint : Checkout step 1 image inside screenshots folder
+
+
+    // Add cloud image and resize the image
+    //Hint : Checkout step 3 image inside screenshots folder
+
+
+    // set random position
+    //Hint : Checkout step 4 image inside screenshots folder
+
+
+    //adjust the depth
+    //Hint : Checkout step 5 image inside screenshots folder
+
+  }
 }
